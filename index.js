4ab6cd85f32c076b207e991446a4ec9b77515216
AMANI-BLU <solomonamanuel66@gmail.com>
2024-10-28 09:33:25 +0300

    challenge done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
 
 var app = express();
 
@@ -11,10 +11,40 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', (req, res) => {
+  let dataBuffer = Buffer.alloc(0);
 
+  // Gather data chunks into dataBuffer
+  req.on('data', (chunk) => {
+    dataBuffer = Buffer.concat([dataBuffer, chunk]);
+  });
 
+  req.on('end', () => {
+    // Convert buffer data into string
+    const dataString = dataBuffer.toString();
+    
+    // Extract file metadata from data string
+    const contentDisposition = dataString.match(/Content-Disposition: form-data; name="upfile"; filename="(.+?)"/);
+    const contentType = dataString.match(/Content-Type: (.+?)(\r\n|\n)/);
+
+    if (!contentDisposition || !contentType) {
+      return res.status(400).json({ error: 'Invalid file upload' });
+    }
+
+    const fileName = contentDisposition[1];
+    const fileType = contentType[1];
+    const fileSize = dataBuffer.length;
+
+    // Respond with the file information
+    res.json({
+      name: fileName,
+      type: fileType,
+      size: fileSize
+    });
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
